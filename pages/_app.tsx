e04bbca91db753299b82ae392283222784c04068
Victor Garcia Mestre <vgmestre@gmail.com>
2022-04-11 22:37:09 +0200

    fix myapp

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,11 @@ import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return;
-  <ChakraProvider>
-    <Component {...pageProps} />
-  </ChakraProvider>;
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
 export default MyApp;
