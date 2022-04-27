271a730ad58e92384387ef273c8926f1392afdcb
Victor Garcia Mestre <vgmestre@gmail.com>
2022-04-27 22:46:17 +0200

    paginated records

--- server/routers/record.ts
+++ server/routers/record.ts
@@ -4,8 +4,41 @@ import { TRPCError } from '@trpc/server';
 import { z } from 'zod';
 import { createRouter } from '../createRouter';
 
-export const recordRouter = createRouter().query('all', {
-  async resolve() {
-    return prisma.record.findMany({ skip: 1, take: 10 });
-  },
-});
+export const recordRouter = createRouter()
+  .query('all', {
+    async resolve() {
+      return prisma.record.findMany({ skip: 1, take: 10 });
+    },
+  })
+  .query('paginated', {
+    input: z.object({
+      skip: z.number().nonnegative().nullish(),
+      take: z.number().positive().nullish(),
+      sortBy: z.string().nullish(),
+    }),
+    async resolve({ input }) {
+      const skip = input.skip ?? 0;
+      const take = input.take ?? 50;
+      //const sortBy = input.sortBy ??
+
+      return prisma.record.findMany({
+        skip,
+        take,
+        orderBy: {
+          title: 'asc',
+        },
+        include: {
+          author: {
+            select: {
+              name: true,
+            },
+          },
+          label: {
+            select: {
+              name: true,
+            },
+          },
+        },
+      });
+    },
+  });
