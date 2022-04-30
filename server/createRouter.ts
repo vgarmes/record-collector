d7779e993a2942f5677e2e8ee14a6fc50b78479e
Victor Garcia Mestre <vgmestre@gmail.com>
2022-04-30 21:15:13 +0200

    protected router helper and refactor user

--- server/createRouter.ts
+++ server/createRouter.ts
@@ -1,9 +1,21 @@
 import { Context } from './context';
 import * as trpc from '@trpc/server';
+import { Role } from '@prisma/client';
+import { TRPCError } from '@trpc/server';
 
 /**
  * Helper function to create a router with context
  */
 export function createRouter() {
   return trpc.router<Context>();
+}
+
+export function createProtectedRouter(role?: Role) {
+  return trpc.router<Context>().middleware(async ({ ctx, next }) => {
+    if (!ctx.session || (role && ctx.session.role !== role)) {
+      throw new TRPCError({ code: 'UNAUTHORIZED' });
+    }
+    // infers that `session` is non-nullable to downstream procedures
+    return next({ ctx: { ...ctx, session: ctx.session } });
+  });
 }
--- server/routers/user.ts
+++ server/routers/user.ts
@@ -2,42 +2,34 @@ import { Prisma } from '@prisma/client';
 import { prisma } from '../prisma';
 import { TRPCError } from '@trpc/server';
 import { z } from 'zod';
-import { createRouter } from '../createRouter';
+import { createProtectedRouter, createRouter } from '../createRouter';
 import { hashPassword, verifyPassword } from '../../utils/auth';
 
-export const userRouter = createRouter()
-  .middleware(async ({ ctx, next }) => {
-    if (!ctx.session) {
-      throw new TRPCError({ code: 'UNAUTHORIZED' });
+export const userRouter = createProtectedRouter().mutation('change-password', {
+  input: z.object({
+    password: z.string().min(1).max(32),
+    newPassword: z.string().min(1).max(32),
+  }),
+  async resolve({ input, ctx }) {
+    const { password, newPassword } = input;
+    const hashedPassword = await hashPassword(password);
+    const user = await prisma.user.findUnique({
+      where: { id: ctx.session.user.id },
+    });
+
+    if (!user || !verifyPassword(hashedPassword, user.password)) {
+      throw new TRPCError({ code: 'BAD_REQUEST' });
     }
-    // infers that `session` is non-nullable to downstream procedures
-    return next({ ctx: { ...ctx, session: ctx.session } });
-  })
-  .mutation('change-password', {
-    input: z.object({
-      password: z.string().min(1).max(32),
-      newPassword: z.string().min(1).max(32),
-    }),
-    async resolve({ input, ctx }) {
-      const { password, newPassword } = input;
-      const hashedPassword = await hashPassword(password);
-      const user = await prisma.user.findUnique({
-        where: { id: ctx.session.user.id },
-      });
 
-      if (!user || !verifyPassword(hashedPassword, user.password)) {
-        throw new TRPCError({ code: 'BAD_REQUEST' });
-      }
+    const newHashedPassword = await hashPassword(newPassword);
 
-      const newHashedPassword = await hashPassword(newPassword);
-
-      await prisma.user.update({
-        where: {
-          id: user.id,
-        },
-        data: {
-          password: newHashedPassword,
-        },
-      });
-    },
-  });
+    await prisma.user.update({
+      where: {
+        id: user.id,
+      },
+      data: {
+        password: newHashedPassword,
+      },
+    });
+  },
+});
