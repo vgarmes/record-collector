e0dbb482f6cf0eade48f407c0ca9ba4d7d308430
Victor Garcia Mestre <vgmestre@gmail.com>
2022-06-03 00:40:32 +0200

    add authors

--- pages/authors/index.tsx
+++ pages/authors/index.tsx
@@ -1,7 +1,51 @@
+import { Button, Flex, Spinner, Text } from '@chakra-ui/react';
 import { NextPage } from 'next';
+import { useSession } from 'next-auth/react';
+import { useState } from 'react';
+import { trpc } from '../../utils/trpc';
 
 const Authors: NextPage = () => {
-  return <>En construccion</>;
+  const pageSize = 20;
+  const [pageIndex, setPageIndex] = useState(0);
+  const { data: session } = useSession();
+  const { data } = trpc.useQuery(
+    ['author.paginated', { skip: pageIndex * pageSize, take: pageSize }],
+    { keepPreviousData: true }
+  );
+
+  if (!data) {
+    return (
+      <Flex justify="center" align="center" height="100vh">
+        <Spinner size="xl" />
+      </Flex>
+    );
+  }
+  console.log(data);
+  return (
+    <div>
+      <ul>
+        {data?.map((author) => (
+          <li key={author.id}>
+            <Flex>
+              <Text pr={2}>{author.name}</Text>
+              <Text>{author._count.records}</Text>
+            </Flex>
+          </li>
+        ))}
+      </ul>
+      <Flex>
+        <Button
+          disabled={pageIndex === 0}
+          onClick={() => setPageIndex((prev) => prev - 1)}
+        >
+          Anterior
+        </Button>
+        <Button onClick={() => setPageIndex((prev) => prev + 1)}>
+          Siguiente
+        </Button>
+      </Flex>
+    </div>
+  );
 };
 
 export default Authors;
--- server/routers/author.ts
+++ server/routers/author.ts
@@ -4,22 +4,53 @@ import { TRPCError } from '@trpc/server';
 import { z } from 'zod';
 import { createProtectedRouter, createRouter } from '../createRouter';
 
-export const authorRouter = createRouter().query('search', {
-  input: z.object({
-    searchQuery: z.string().optional(),
-  }),
-  async resolve({ input }) {
-    console.log(input);
-    return prisma.author.findMany({
-      where: {
-        name: {
-          contains: input.searchQuery,
+export const authorRouter = createRouter()
+  .query('paginated', {
+    input: z.object({
+      skip: z.number().nonnegative().nullish(),
+      take: z.number().positive().nullish(),
+      searchQuery: z.string().optional(),
+    }),
+    async resolve({ input }) {
+      const skip = input.skip ?? 0;
+      const take = input.take ?? 50;
+      const where: Prisma.AuthorWhereInput | undefined = input.searchQuery
+        ? { name: { contains: input.searchQuery } }
+        : undefined;
+
+      return prisma.author.findMany({
+        skip,
+        take,
+        where,
+        include: {
+          _count: {
+            select: {
+              records: true,
+            },
+          },
         },
-      },
-      orderBy: {
-        name: 'asc',
-      },
-      take: 10,
-    });
-  },
-});
+        orderBy: {
+          name: 'asc',
+        },
+      });
+    },
+  })
+  .query('search', {
+    input: z.object({
+      searchQuery: z.string().optional(),
+    }),
+    async resolve({ input }) {
+      console.log(input);
+      return prisma.author.findMany({
+        where: {
+          name: {
+            contains: input.searchQuery,
+          },
+        },
+        orderBy: {
+          name: 'asc',
+        },
+        take: 10,
+      });
+    },
+  });
